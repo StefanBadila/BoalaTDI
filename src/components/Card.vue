4e545620c3dd1c5ac61497fe70d9e83fceeae10c
StefanBadila <noname_stefan@yahoo.com>
2023-03-26 20:20:00 +0300

    match condition

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,10 @@
 <script>
 export default{
 props:{
+    matched: {
+        type: Boolean,
+        default: false
+    },
     position: {
         type: Number,
         required: true
@@ -18,7 +22,8 @@ visible: {
 setup(props, context) {
     const selectCard = () => {
         context.emit('select-card', {
-            position: props.position
+            position: props.position,
+            faceValue: props.value
         })
     }
     return {
@@ -31,7 +36,7 @@ setup(props, context) {
 <template>
 <div class="card" @click="selectCard">
         <div v-if="visible" class="card-face is-front">
-        {{ value }}
+        {{ value }} --{{ matched }}
     </div> 
     <div v-else class="card-face is-back">
      Back
