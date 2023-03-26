4211fcab4214e148d1753cbc0db7ab4642c95d45
Bican-Valeriu-Cristian <cristibicann@gmail.com>
2023-03-26 18:04:56 +0300

    prima

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
-<script setup>
-
-</script>
-
 <template>
-    <body>
-      <h1>Salut</h1>
-      
-    </body>
+ <h1>BoalaTDI</h1>
+<br>
+<section class="game-board">
+  <Card v-for="(card,index) in cardList" 
+  :key="'card-${index}'"
+  :value="card"/>
+</section>
 </template>
 
-<style scoped>
+<script>
+import Card from './components/Card.vue'
 
+export default{
+  name: 'App'
+  components: {
+    Card
+  },
+  setup () {
+    const cardList = []
+
+    for (let i=0; i<16;i++){
+      cardList.push(i)
+    }
+    return{
+      cardList
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+font-family: Arial, Helvetica, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smotthing: grayscale;
+text-align: center;
+color: chartreuse;
+margin-top: 60px;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board{
+  display:grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+props:{
+    
+}
+}
+</script>
+
+<template>
+    <div class="card">
+        Card
+    </div>
+    
+</template>
