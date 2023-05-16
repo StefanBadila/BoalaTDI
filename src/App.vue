873b87a14e5d39259fff56a819570feea8e1684d
StefanBadila <noname_stefan@yahoo.com>
2023-05-16 18:09:31 +0300

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -87,13 +87,13 @@ export default{
       }
     })
     
-    const flipCard = (payload) => {
+    const flipCard = payload => {
      
-      cardList.value[payload.position].visible = true
+      cardList.value[payload.position].visible = true;
 
       if(userSelection.value[0]){
-        if (userSelection.value[0].position === payload.position && userSelection.value[0].faceValue === payload.faceValue ){
-        return 
+        if ((userSelection.value[0].position === payload.position) && (userSelection.value[0].faceValue === payload.faceValue) ){
+        return
       }else{
         userSelection.value[1]=payload
       }
@@ -104,18 +104,22 @@ export default{
     watch(userSelection, 
     currentValue => {
       if(currentValue.length === 2){
-        const cardOne = currentValue[0]
-        const cardTwo = currentValue[1]
+        const cardOne = currentValue[0];
+        const cardTwo = currentValue[1];
+
+        
 
         if(cardOne.faceValue === cardTwo.faceValue){
-            cardList.value[cardOne.position].matched=true
-            cardList.value[cardTwo.position].matched=true
+            cardList.value[cardOne.position].matched=true;
+            cardList.value[cardTwo.position].matched=true;
         }
         else{
 
-          seTimeout(()=>{cardList.value[cardOne.position].matched=false
-          cardList.value[cardTwo.position].matched=false
-          }, 2000)
+          setTimeout(() => {
+            cardList.value[cardOne.position].visible=false;
+            cardList.value[cardTwo.position].visible=false;
+          },750)
+        
         }
 
 
@@ -131,7 +135,6 @@ export default{
       flipCard,
       userSelection,
       status,
-      remainingPairs,
       shuffleCards,
       restartGame
     }
